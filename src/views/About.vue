85161000710dc8100fa0ccffc61a9f28c8820349
leonixyz <leonixyz@gmail.com>
2018-10-02 20:24:24 +0200

    parameterize everything

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,42 +5,33 @@
         <p id="contacts">
             <span v-html="mailLink"></span>
             &nbsp;
-            <a hreflang="en" href="https://github.com/leonixyz"><span class="fab fa-github"></span><span class="sr-only">Github</span></a>
-            &nbsp;
-            <a hreflang="en" href="https://www.linkedin.com/in/giulio-roman-857557144/"><span class="fab fa-linkedin"></span><span class="sr-only">Linkedin</span></a>
+            <span v-for="contact in contacts" :key="contact.url">
+              <a :href="contact.url" hreflang="en">
+                <span :class="contact.icon"></span>
+                <span class="sr-only">{{ contact.name }}</span>
+              </a>
+              &nbsp;
+            </span>
         </p>
-        <h1>Giulio Roman</h1>
+        <h1>{{ myName }}</h1>
         <p>
-            <em>
-                <span class="sr-only">about me:</span>
-                Web development enthusiast,<br>
-                Free Software advocate,<br>
-                GNU/Linux lover.
-            </em>
+            <span class="sr-only">about me:</span>
+            <em v-html="aboutMe"></em>
         </p>
         <p>
             <span class="sr-only">languages I speak:</span>
-            <span class="language"><img class="flag" src="img/flags/it.svg" alt="Italian"> native</span>
-            <span class="language"><img class="flag" src="img/flags/gb.svg" alt="English"> excellent</span>
-            <span class="language"><img class="flag" src="img/flags/de.svg" alt="German"> good</span>
-            <span class="language"><img class="flag" src="img/flags/es.svg" alt="Spanish"> good</span>
+            <span v-for="language in languages" :key="language.locale" class="language">
+              <img class="flag" :src="`img/flags/${language.locale}.svg`" :alt="language.name">
+              good
+            </span>
         </p>
     </div>
     <div id="skills">
         <p>
             <span class="sr-only">my skills:</span>
-            <a class="hashtag" href="https://angular.io/" target="_blank">Angular</a>
-            <a class="hashtag" href="https://angularjs.org/" target="_blank">AngularJs</a>
-            <a class="hashtag" href="https://aws.amazon.com/" target="_blank">AWS</a>
-            <a class="hashtag" href="https://www.asp.net/" target="_blank">ASP.NET</a>
-            <a class="hashtag" href="https://www.w3.org/Style/CSS/" target="_blank">CSS</a>
-            <a class="hashtag" href="https://www.docker.com/" target="_blank">Docker</a>
-            <a class="hashtag" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">Javascript</a>
-            <a class="hashtag" href="https://kubernetes.io/" target="_blank">Kubernetes</a>
-            <a class="hashtag" href="https://nodejs.org/" target="_blank">NodeJs</a>
-            <a class="hashtag" href="https://secure.php.net/" target="_blank">PHP</a>
-            <a class="hashtag" href="https://wordpress.org/" target="_blank">WordPress</a>
-            <a class="hashtag" href="https://www.silverstripe.org/" target="_blank">Silverstripe</a>
+            <a v-for="skill in skills" :key="skill.url" :href="skill.url" class="hashtag" target="_blank">
+              {{ skill.name }}
+            </a>
         </p>
     </div>
   </main>
@@ -54,7 +45,7 @@
     margin-left: 1rem;
   }
 
-  #showcase {
+    #showcase {
     text-align: right;
   }
 
@@ -124,7 +115,93 @@ export default {
   name: 'app',
   data: () => {
     return {
-      mailLink: "<a href='mailto:&#109;&#101;&#64;&#108;&#101;&#111;&#110;&#105;&#120;&#121;&#122;&#46;&#110;&#101;&#116;'><span class='fa fa-at'></span><span class='sr-only'>Email</span></a>"
+      myName: 'Giulio Roman',
+      aboutMe: 'Web development enthusiast,<br>Free Software advocate,<br>GNU/Linux lover.',
+      mailLink: '<a href="mailto:&#109;&#101;&#64;&#108;&#101;&#111;&#110;&#105;&#120;&#121;&#122;&#46;&#110;&#101;&#116;"><span class="fa fa-at"></span><span class="sr-only">Email</span></a>',
+      contacts: [
+        {
+          name: 'Github',
+          url: 'https://github.com/leonixyz',
+          icon: 'fab fa-github'
+        },
+        {
+          name: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/giulio-roman-857557144/',
+          icon: 'fab fa-linkedin'
+        }
+      ],
+      languages: [
+        {
+          name: 'Italian',
+          locale: 'it',
+          level: 'native'
+        },
+        {
+          name: 'English',
+          locale: 'gb',
+          level: 'excellent'
+        },
+        {
+          name: 'German',
+          locale: 'de',
+          level: 'good'
+        },
+        {
+          name: 'Spanish',
+          locale: 'es',
+          level: 'good'
+        }
+      ],
+      skills: [
+        {
+          name: 'Angular',
+          url: 'https://angular.io/'
+        },
+        {
+          name: 'AngularJs',
+          url: 'https://angularjs.org/'
+        },
+        {
+          name: 'AWS',
+          url: 'https://aws.amazon.com/'
+        },
+        {
+          name: 'ASP.NET',
+          url: 'https://www.asp.net/'
+        },
+        {
+          name: 'CSS',
+          url: 'https://www.w3.org/Style/CSS/'
+        },
+        {
+          name: 'Docker',
+          url: 'https://www.docker.com/'
+        },
+        {
+          name: 'Javascript',
+          url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+        },
+        {
+          name: 'Kubernetes',
+          url: 'https://kubernetes.io/'
+        },
+        {
+          name: 'NodeJs',
+          url: 'https://nodejs.org/'
+        },
+        {
+          name: 'PHP',
+          url: 'https://secure.php.net/'
+        },
+        {
+          name: 'WordPress',
+          url: 'https://wordpress.org/'
+        },
+        {
+          name: 'Silverstripe',
+          url: 'https://www.silverstripe.org/'
+        }
+      ]
     }
   }
 }
