387bccf7b363fc444be74c48ed63caf1dfe9c7c5
leonixyz <leonixyz@gmail.com>
2018-10-02 21:29:05 +0200

    prepare to fetch blog posts from github

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <main>
-    <post title="Hello World!" date="2018-10-02"/>
+    <post v-for="post in getPosts()" :key="post.title" :title="post.title" :date="post.date"/>
   </main>
 </template>
 
 <script>
 // @ is an alias to /src
 import Post from '@/components/Post.vue'
+import * as conf from '../../siteconfig.json'
 
 export default {
   name: 'home',
   components: {
     Post
+  },
+  methods: {
+    getPosts: () => {
+      console.log('im gonna fetch')
+      console.log(`https://api.github.com/repos/${conf.repo.owner}/${conf.repo.name}/contents/articles`)
+      return [{ title: 'Hello World!', date: '2018-10-02' }]
+    }
   }
 }
 </script>
