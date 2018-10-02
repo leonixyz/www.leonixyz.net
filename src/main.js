1da1ec7a78bb8740a34b408c70c476c780e886f8
leonixyz <leonixyz@gmail.com>
2018-10-02 21:35:50 +0200

    install vue-resource

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Resource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(Resource)
 
 new Vue({
   router,
