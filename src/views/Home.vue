d782d0e75ddf14e13ef9a0dea69c16ce42ebe025
leonixyz <leonixyz@gmail.com>
2018-10-02 23:03:51 +0200

    use name convention for posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <post v-for="post in getPosts()" :key="post.title" :title="post.title" :date="post.date"/>
+    <post v-for="post in posts" :key="post.title" :title="post.title" :date="post.date"/>
   </main>
 </template>
 
@@ -11,20 +11,35 @@ import * as conf from '../../siteconfig.json'
 
 export default {
   name: 'home',
+  data: function () {
+    return {
+      posts: []
+    }
+  },
   components: {
     Post
+  },
+  created: function () {
+    this.getPosts()
   },
   methods: {
     getPosts: function () {
       this.$http.get(`https://api.github.com/repos/${conf.repo.owner}/${conf.repo.name}/contents/articles`)
         .then(response => {
-          // get body data
-          console.log(response.body)
+          if (response.body) {
+            response.body.forEach(element => {
+              const rawName = element.name.replace(/.md$/g, '')
+              const date = rawName.split('_')[0]
+              const title = rawName.split('_')[1]
+              this.posts.push({
+                title: title,
+                date: new Date(date)
+              })
+            })
+          }
         }, response => {
-          console.error('you fucked up everything')
+          console.error('you fucked up everything as usual')
         })
-
-      return [{ title: 'Hello World!', date: '2018-10-02' }]
     }
   }
 }
